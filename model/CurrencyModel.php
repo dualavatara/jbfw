ee91a0fc96989ca62fddb441bcc7e75cfdc76789
Konstantin Zhukov <dualavatara@gmail.com>
2012-02-28 05:12:59 +0400

    Currency model and admin

---
+++ model/CurrencyModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: zhukov
+ * Date: 28.02.12
+ * Time: 4:04
+ */
+
+require_once 'lib/model.lib.php';
+
+class CurrencyModel extends Model {
+	/**
+	 * @param IDatabase $db
+	 */
+	public function __construct(IDatabase $db) {
+		parent::__construct('currency', $db);
+		$this->field('name', new CharField('name'));
+		$this->field('sign', new CharField('sign'));
+		$this->field('course', new RealField('course'));
+	}
+
+}
