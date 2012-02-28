c30fe43fffe445604fb2ec91f6d56df706583944
Konstantin Zhukov <dualavatara@gmail.com>
2012-02-28 04:06:48 +0400

    Database test util

--- tests/bootstrap.sample.php
+++ tests/bootstrap.sample.php
@@ -1,4 +1,20 @@
 <?
 set_include_path(get_include_path().':/path/to/project');
 get_include_path();
+
+require_once("config/config.php");
+
+$user = DB_USER;
+$password = DB_PASS;
+$dbname = DB_NAME;
+
+$testuser = "ivanov";
+$testpassword = "123";
+$testdbname = "test_".DB_NAME; //database should exist before running tests
+
+$out = `mysql -u $testuser -p$testpassword $testdbname -e "show tables" | grep -v Tables_in | grep -v "+" | awk '{print "drop table " $1 ";"}' | mysql -u $testuser -p$testpassword $testdbname`;
+$out = `mysqldump -u$user -p$password --no-data $dbname > copytotest.sql`;
+$out = `mysql -u $testuser -p$testpassword $testdbname < copytotest.sql`;
+
+print $out;
 ?>
