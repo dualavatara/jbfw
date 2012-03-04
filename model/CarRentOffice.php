9056b2b72ca9be939b2cc7760c1eae298ff20c60
Konstantin Zhukov <dualavatara@gmail.com>
2012-03-04 10:02:40 +0400

    jbfw-10: Объект Конторы по прокату авто

--- model/CarRentOffice.php
+++ model/CarRentOffice.php
@@ -10,5 +10,11 @@ require_once 'lib/model.lib.php';
 class CarRentOffice extends Model {
 	public function __construct(IDatabase $db) {
 		parent::__construct('car_rent_office', $db);
+
+		$this->field(new CharField('name'));
+		$this->field(new CharField('description'));
+		$this->field(new RealField('percent'));
+		$this->field(new IntField('customer_id'));
+		$this->field(new RatingField('customer_id'));
 	}
 }
