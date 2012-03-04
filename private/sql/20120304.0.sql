3fc87dcfc0f6641f1321430188b3af806d23eb73
Konstantin Zhukov <dualavatara@gmail.com>
2012-03-04 07:28:21 +0400

    jbfw-9: Объект Курорт

--- private/sql/20120304.0.sql
+++ private/sql/20120304.0.sql
@@ -1,3 +1,15 @@
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+
 --
 -- Dumping data for table `settings`
 --
@@ -15,3 +27,15 @@ INSERT INTO `settings` (`id`, `name`, `value`) VALUES(10, 'Описание пр
 INSERT INTO `settings` (`id`, `name`, `value`) VALUES(11, 'Название проекта', '');
 INSERT INTO `settings` (`id`, `name`, `value`) VALUES(12, 'Количество предложений по объектам на 1 страницу', '');
 INSERT INTO `settings` (`id`, `name`, `value`) VALUES(13, 'Текст для CEO', '');
+
+--
+-- Table structure for table `resort`
+--
+
+CREATE TABLE IF NOT EXISTS `resort` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext NOT NULL,
+  `link` tinytext NOT NULL,
+  `gmaplink` tinytext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
