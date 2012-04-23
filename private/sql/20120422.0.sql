abb353712dbe64d3e4637ceb689c83a3f6dc951c
Zhukov Konstantin <dualavatara@gmail.com>
2012-04-23 18:13:37 +0400

    search

--- private/sql/20120422.0.sql
+++ private/sql/20120422.0.sql
@@ -1,15 +1,16 @@
+DROP TABLE `jbfw`.`realty_type`;
 CREATE  TABLE `jbfw`.`realty_type` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` TINYTEXT NOT NULL ,
-  PRIMARY KEY (`id`) );
+  PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;;
 
 ALTER TABLE `jbfw`.`realty_type` ADD COLUMN `flags` INT NULL  AFTER `name` ;
 
-
+DROP TABLE `jbfw`.`appartment_type`;
 CREATE  TABLE `jbfw`.`appartment_type` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` TINYTEXT NOT NULL ,
-  PRIMARY KEY (`id`) );
+  PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;;
 
 ALTER TABLE `jbfw`.`appartment_type` ADD COLUMN `flags` INT NULL  AFTER `name` ;
 
